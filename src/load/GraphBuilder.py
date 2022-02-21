03da143af61a39c6de3c03d7614b71c5a2085d91
Zuye Zheng <zuyezheng@gmail.com>
2022-02-20 20:04:49 -0800

    Graph with aggregate edges.

--- src/load/GraphBuilder.py
+++ src/load/GraphBuilder.py
@@ -47,11 +47,43 @@ class GraphBuilder:
         self._client = client
         self._n_workers = n_workers
 
-    def entities_from_transfers(self, transfers_glob: str, transfers_format: StorageFormat, destination_dir: str):
-        """ Load transfers and extract out unique entities to create. """
+    def create_artifacts(
+        self,
+        transfers_glob: str,
+        transfers_format: StorageFormat,
+        destination_dir: str,
+        # span in seconds used to bucket transfers for aggregation
+        bucket_span: int
+    ):
+        """ Load transfers and compute the artifacts used for vertices and edges to store in a graph. """
         destination_path = Path(destination_dir)
 
         df = transfers_format.read_file(transfers_glob).repartition(npartitions=self._n_workers)
+
+        # aggregate transfers so edges between addresses are less noisy
+        aggregate_transfers = df.assign(
+            # make creating a count of rows column simpler
+            size=1,
+            # create buckets for time
+            time_bucket=(df.time // bucket_span) * bucket_span,
+            earliest=df.time,
+            latest=df.time
+        )
+        # aggregate by unique tuples per coin/token and by given time span bucket
+        aggregate_transfers = aggregate_transfers.groupby(['source', 'destination', 'mint', 'time_bucket'])
+        aggregate_transfers = aggregate_transfers.agg({
+            'value': 'sum',
+            'scale': 'first',
+            'earliest': 'min',
+            'latest': 'max',
+            'size': 'sum'
+        })
+        # convert the value to a float in the right scale as an approximation due to overflows
+        aggregate_transfers = aggregate_transfers.assign(
+            value_approx=aggregate_transfers.value / 10**aggregate_transfers.scale
+        )
+        aggregate_transfers = aggregate_transfers.sort_values('size', ascending=False).reset_index()
+
         dask.compute(
             StorageFormat.CSV.to_file(
                 df.source.append(df.destination).unique().rename('hash'),
@@ -60,14 +92,18 @@ class GraphBuilder:
             StorageFormat.CSV.to_file(
                 df.mint.unique().rename('hash'),
                 str(destination_path.joinpath('mints'))
-            )
+            ),
+            StorageFormat.CSV.to_file(
+                aggregate_transfers,
+                str(destination_path.joinpath('aggregated_transfers'))
+            ),
         )
 
-    def create_vertices(self, vertices_dir: str):
+    def create_graph(self, artifacts_dir: str):
         """ Create the vertices to join with edges later. """
-        vertices_path = Path(vertices_dir)
+        artifacts_path = Path(artifacts_dir)
 
-        def create(name, file, tx):
+        def create_vertices(name, file, tx):
             tx.run(
                 f"""
                     LOAD CSV WITH HEADERS FROM 'file:///{file}' AS line
@@ -81,18 +117,7 @@ class GraphBuilder:
         def create_index(name, tx):
             tx.run(f'CREATE INDEX {name}_hash IF NOT EXISTS FOR (n:{name}) ON (n.hash)')
 
-        with self._driver.session() as session:
-            for vertex_name, file_prefix in [['Address', 'addresses'], ['Mint', 'mints']]:
-                session.write_transaction(partial(
-                    create, vertex_name, str(vertices_path.joinpath(f'{file_prefix}.csv'))
-                ))
-
-                session.write_transaction(partial(
-                    create_index, vertex_name
-                ))
-
-    def create_transfer_edges(self, transfers_csv_path: str):
-        def create(file, tx):
+        def create_edges(file, tx):
             tx.run(
                 f"""
                     CALL apoc.periodic.iterate(
@@ -104,27 +129,34 @@ class GraphBuilder:
                             (mint:Mint {{hash: line.mint}})
                         CREATE 
                             (source)-[:transfer {{
-                                value: toInteger(line.value),
+                                mint: line.mint,
+                                value: line.value,
                                 scale: toInteger(line.scale),
-                                time: toInteger(line.time),
-                                transaction: line.transaction,
-                                blockhash: line.blockhash
+                                value_approx: toFloat(line.value_approx),
+                                time_bucket: toInteger(line.time_bucket),
+                                earliest: toInteger(line.earliest),
+                                latest: toInteger(line.latest),
+                                size: toInteger(line.size)
                             }}]->(destination),
                             (source)-[:mint {{
                                 role: "source",
-                                value: toInteger(line.value),
+                                value: line.value,
                                 scale: toInteger(line.scale),
-                                time: toInteger(line.time),
-                                transaction: line.transaction,
-                                blockhash: line.blockhash
+                                value_approx: toFloat(line.value_approx),
+                                time_bucket: toInteger(line.time_bucket),
+                                earliest: toInteger(line.earliest),
+                                latest: toInteger(line.latest),
+                                size: toInteger(line.size)
                             }}]->(mint),
                             (destination)-[:mint {{
                                 role: "destination",
-                                value: toInteger(line.value),
+                                value: line.value,
                                 scale: toInteger(line.scale),
-                                time: toInteger(line.time),
-                                transaction: line.transaction,
-                                blockhash: line.blockhash
+                                value_approx: toFloat(line.value_approx),
+                                time_bucket: toInteger(line.time_bucket),
+                                earliest: toInteger(line.earliest),
+                                latest: toInteger(line.latest),
+                                size: toInteger(line.size)
                             }}]->(mint)
                         ',
                         {{batchSize:100000, iterateList:true, parallel:false}}
@@ -133,17 +165,31 @@ class GraphBuilder:
             )
 
         with self._driver.session() as session:
-            session.write_transaction(partial(create, transfers_csv_path))
+            # create the vertices and indices for them
+            for vertex_name, file_prefix in [['Address', 'addresses'], ['Mint', 'mints']]:
+                session.write_transaction(partial(
+                    create_vertices, vertex_name, str(artifacts_path.joinpath(f'{file_prefix}.csv'))
+                ))
+
+                session.write_transaction(partial(
+                    create_index, vertex_name
+                ))
+
+            # create the edges
+            session.write_transaction(partial(
+                create_edges, str(artifacts_path.joinpath('aggregated_transfers.csv'))
+            ))
 
 
 if __name__ == '__main__':
     with GraphBuilder.with_local_cluster('', uri='bolt://localhost:7687', user='neo4j', password='test12345') as graph_builder:
         start = time.perf_counter()
-        graph_builder.entities_from_transfers(
-            '/mnt/scratch_raid/solana/streaming/120470000_transfers.csv',
+        graph_builder.create_artifacts(
+            '/mnt/storage/datasets/sol/streaming/120470000_transfers.csv',
             StorageFormat.CSV,
-            '/mnt/scratch_raid/solana/graph/120470000/vertices'
+            '/mnt/scratch_raid/solana/graph/120470000/artifacts',
+            # bucket per hour
+            60*60
         )
-        graph_builder.create_vertices('/graph/120470000/vertices')
-        graph_builder.create_transfer_edges('/streaming/120470000_transfers.csv')
+        graph_builder.create_graph('/graph/120470000/artifacts')
         print((time.perf_counter() - start))
